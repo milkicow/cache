d7d33a1a3d5639988d36dbc9378c7f59306eaf40
milkicow <kozlov.aleksandr@phystech.edu>
2022-09-26 17:36:44 +0300

    timer was commented

--- cache.cpp
+++ cache.cpp
@@ -4,13 +4,13 @@ int main()
 {   
     int size = 0, number_of_pages = 0;
     std::cin >> size >> number_of_pages;
-    using namespace std::chrono;
-    using fseconds = duration<float>;
+    //using namespace std::chrono;
+    //using fseconds = duration<float>;
 
     lfu::cache<page_t> cache(size);
 
     int hits = 0;
-    auto start = system_clock::now();
+    //auto start = system_clock::now();
     for (int i = 0; i < number_of_pages; i++)
     {
         int page_number = 0;
@@ -18,8 +18,8 @@ int main()
         if (cache.lookup_update(page_number, slow_get_page)) hits++;
     }
 
-    auto time = system_clock::now() - start;
-    std::cout << "runtime " << duration_cast<fseconds>(time).count() << "c\n";
+    //auto time = system_clock::now() - start;
+    //std::cout << "runtime " << duration_cast<fseconds>(time).count() << "c\n";
 
     std::cout << hits << '\n';
 }
