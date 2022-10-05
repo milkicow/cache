6de73f7c0460ddbb2b6de3ea6c7d221d00668fac
milkicow <kozlov.aleksandr@phystech.edu>
2022-10-05 18:31:01 +0300

    u can run tests from build not from bin

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -6,42 +6,42 @@ extern int perfect(std::ifstream &file);
 
 TEST(TestLFU_cache, TestNumber0)
 {   
-    std::ifstream file("../../test/TestLFU0.txt");
+    std::ifstream file("../test/TestLFU0.txt");
     EXPECT_EQ(LFU(file), 5);
     file.close();
 }
 
 TEST(TestLFU_cache, TestNumber1)
 {
-    std::ifstream file("../../test/TestLFU1.txt");
+    std::ifstream file("../test/TestLFU1.txt");
     EXPECT_EQ(LFU(file), 2);
     file.close();
 }
 
 TEST(TestLFU_cache, TestNumber2)
 {
-    std::ifstream file("../../test/TestLFU2.txt");
+    std::ifstream file("../test/TestLFU2.txt");
     EXPECT_EQ(LFU(file), 3);
     file.close();
 }
 
 TEST(TestPerfect_cache, TestNumber0)
 {
-    std::ifstream file("../../test/Testperf0.txt");
+    std::ifstream file("../test/Testperf0.txt");
     EXPECT_EQ(perfect(file), 5);
     file.close();
 }
 
 TEST(TestPerfect_cache, TestNumber1)
 {
-    std::ifstream file("../../test/Testperf1.txt");
+    std::ifstream file("../test/Testperf1.txt");
     EXPECT_EQ(perfect(file), 3);
     file.close();
 }
 
 TEST(TestPerfect_cache, TestNumber2)
 {
-    std::ifstream file("../../test/Testperf2.txt");
+    std::ifstream file("../test/Testperf2.txt");
     EXPECT_EQ(perfect(file), 4);
     file.close();
 }
